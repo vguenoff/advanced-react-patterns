96dd7c236a171065d9138bfdcc06367543093c28
vguenoff <vguenoff@gmail.com>
2022-08-15 12:31:13 +0300

    Prettier format

--- src/final/03.js
+++ src/final/03.js
@@ -2,53 +2,53 @@
 // http://localhost:3000/isolated/final/03.js
 
 import * as React from 'react'
-import {Switch} from '../switch'
+import { Switch } from '../switch'
 
 const ToggleContext = React.createContext()
 ToggleContext.displayName = 'ToggleContext'
 
-function Toggle({children}) {
-  const [on, setOn] = React.useState(false)
-  const toggle = () => setOn(!on)
+function Toggle({ children }) {
+    const [on, setOn] = React.useState(false)
+    const toggle = () => setOn(!on)
 
-  return (
-    <ToggleContext.Provider value={{on, toggle}}>
-      {children}
-    </ToggleContext.Provider>
-  )
+    return (
+        <ToggleContext.Provider value={{ on, toggle }}>
+            {children}
+        </ToggleContext.Provider>
+    )
 }
 
 function useToggle() {
-  return React.useContext(ToggleContext)
+    return React.useContext(ToggleContext)
 }
 
-function ToggleOn({children}) {
-  const {on} = useToggle()
-  return on ? children : null
+function ToggleOn({ children }) {
+    const { on } = useToggle()
+    return on ? children : null
 }
 
-function ToggleOff({children}) {
-  const {on} = useToggle()
-  return on ? null : children
+function ToggleOff({ children }) {
+    const { on } = useToggle()
+    return on ? null : children
 }
 
-function ToggleButton({...props}) {
-  const {on, toggle} = useToggle()
-  return <Switch on={on} onClick={toggle} {...props} />
+function ToggleButton({ ...props }) {
+    const { on, toggle } = useToggle()
+    return <Switch on={on} onClick={toggle} {...props} />
 }
 
 function App() {
-  return (
-    <div>
-      <Toggle>
-        <ToggleOn>The button is on</ToggleOn>
-        <ToggleOff>The button is off</ToggleOff>
+    return (
         <div>
-          <ToggleButton />
+            <Toggle>
+                <ToggleOn>The button is on</ToggleOn>
+                <ToggleOff>The button is off</ToggleOff>
+                <div>
+                    <ToggleButton />
+                </div>
+            </Toggle>
         </div>
-      </Toggle>
-    </div>
-  )
+    )
 }
 
 export default App
